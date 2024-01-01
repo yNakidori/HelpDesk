cf61cfe64d165e485a3812070f1a1dfcfdcbc4ec
naki <leandroreisubi@gmail.com>
2024-01-01 19:02:50 -0300

    assets

--- new_help/src/App.js
+++ new_help/src/App.js
@@ -1,14 +1,23 @@
-
 import React from 'react';
 import './styles.css';
-
 import Sigin from './pages/Signin';
-import Footer from './components/Footer';
+import Footer from './components/Footer'
 
 function App() {
+
+  const [darkMode, setDarkMode] = React.useState(true);
+  function toggleDarkMode() {
+    setDarkMode(prevDarkMode => !prevDarkMode)
+  }
   return (
-    <div className="bg-blue-500 min-h-screen flex flex-col items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <nav className="grid grid-cols-2 w-full place-content-between items-center">
+            <h4>Rev 1.0</h4>
+            <img src="./assets/toggle-icon-dark.svg" />
+        </nav>
+      <div className="min-h-screen flex flex-col items-center justify-center">
       <Sigin />
+      </div>
       <Footer />
     </div>
   );
