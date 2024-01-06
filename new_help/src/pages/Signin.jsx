fbe9cee3ca4ebabdc584451b773de493437bad64
naki <leandroreisubi@gmail.com>
2024-01-06 13:00:18 -0300

    new page code

--- new_help/src/pages/Signin.jsx
+++ new_help/src/pages/Signin.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const Signin = () => {
+const Signin = ({ onLogin }) => {
+  const handleLoginClick = () => {
+    onLogin();
+  };
+
     return (
         <section className="gradient-form h-screen bg-neutral-200 dark:bg-neutral-700">
         <div className="container h-4/5 p-10">
@@ -59,7 +63,7 @@ const Signin = () => {
   
                         {/* Submit button */}
                         <div className="mb-12 pb-1 pt-1 text-center">
-                          <button
+                          <button onCLick={handleLoginClick}
                             className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
                             type="button"
                             data-te-ripple-init
